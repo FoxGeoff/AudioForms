c644efcbb28c93aa6514080142cecf672baed551
FoxGeoff <fox.geoffrey@gmail.com>
2018-07-27 18:07:16 -0700

    Add  Off-Canvas Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Audio Impact Admin';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    $(".openNav").click(function(){
+       openNav();
+    })
+
+    $(".closeNav").click(function(){
+      closeNav();
+   })
+
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
+
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+    }
+  }
+  title = 'Audio Impact Administration';
 }
+
+
