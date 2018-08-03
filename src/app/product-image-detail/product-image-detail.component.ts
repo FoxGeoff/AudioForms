c8c70e287e027d434c0543f2ef8c13611ae42124
FoxGeoff <fox.geoffrey@gmail.com>
2018-08-03 16:19:40 -0700

    Add back button

--- src/app/product-image-detail/product-image-detail.component.ts
+++ src/app/product-image-detail/product-image-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import {Location} from '@angular/common';
 
 import { ProductImageService } from '../shared/product-image.service';
 import { ProductImage } from '../shared/productImage';
@@ -12,11 +13,15 @@ import { ProductImage } from '../shared/productImage';
 export class ProductImageDetailComponent implements OnInit {
   productImage: ProductImage;
 
-  constructor(private route: ActivatedRoute, private productImageService: ProductImageService) { }
+  constructor(private route: ActivatedRoute, private productImageService: ProductImageService, private location: Location) { }
 
   ngOnInit() {
     const prodId: number = parseInt(this.route.snapshot.params['productImageId'], 10);
     this.productImage = this.productImageService.getProductById(prodId);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
